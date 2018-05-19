d35842b3803b7311d83917bbe0f64748549858fb
Atif Imam <mohd.atifimam@hotmail.com>
2018-05-20 04:07:11 +0530

    Reading complete content of a file using read system call

--- Read_call_for_reading_complete_content_of_file.c
+++ Read_call_for_reading_complete_content_of_file.c
@@ -2,15 +2,17 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<string.h>
 
 int main(){
     int fd;
-    char buf[2];
+    char buf[1];
     int ret_in,ret_out;
      fd=open("FILE_IO_FUNCTIONS.c",O_RDONLY);
     if(fd>0){
         while(ret_in=read(fd,&buf,sizeof(buf))>0){
-            printf("%s",buf);
+            //printf("%s",buf);
+            write(1,buf,sizeof(buf));
             
         }
 
