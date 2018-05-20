b2c6339d8f8667998991e45d425dcc4296bb7e8e
Atif Imam <mohd.atifimam@hotmail.com>
2018-05-20 12:37:06 +0530

    Write Call to copy contents of 1 fiel to another file

---
+++ write_call_2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main(){
+    int fd;
+    fd=open("open_call.c",O_RDONLY);
+    if(fd==-1){
+        puts("Error Opening file0");
+        exit(0);        
+    }
+    
+    void *buf=(char*)malloc(120);
+    // int count =read(fd,buf,120);
+    // printf("count : %d\n",count);
+    // printf("%s \n",buf);
+    int fd2;
+    fd2=open("new_file1",O_WRONLY|O_CREAT|O_TRUNC,0644);
+
+    if(fd2==-1){
+        puts("Error opening file1");
+        exit(0);
+    }
+    int c;
+    while(read(fd,buf,sizeof(buf))>0){
+        c=write(fd2,buf,sizeof(buf));
+    }
+    if(c==-1){
+        puts("Error Writing file2");
+        exit(0);
+    }
+    close(fd2);
+}
